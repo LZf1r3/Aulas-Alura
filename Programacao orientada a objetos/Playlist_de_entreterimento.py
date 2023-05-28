dc135ec413d4674bbea9b173ce86db7e3a708403
LZf1r3 <lucas.mfaiad@gmail.com>
2023-05-28 00:21:41 -0700

    Update Playlist_de_entreterimento.py

--- Programacao orientada a objetos/Playlist_de_entreterimento.py
+++ Programacao orientada a objetos/Playlist_de_entreterimento.py
@@ -1,5 +1,5 @@
 class Programa:
-    def __init__(self,nome,ano):
+    def __init__(self, nome, ano):
         self._nome = nome.title()
         self.ano = ano
         self._likes = 0
@@ -7,52 +7,62 @@ class Programa:
     @property
     def likes(self):
         return self._likes
-    
+
     @property
     def nome(self):
         return self._nome
-    
+
     @nome.setter
     def nome(self, novo_nome):
         self_nome = novo_nome.title()
-    
+
     def adicionar_like(self):
         self._likes += 1
 
     def __str__(self):
         return f'{self.nome} - {self.ano} - {self.likes} Likes'
+
+
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
-        super().__init__(nome,ano)
+        super().__init__(nome, ano)
         self.duracao = duracao
 
     def __str__(self):
         return f'{self.nome} - {self.ano} - {self.duracao} min - {self.likes} Likes'
 
-class Playlist:
-    def __init__(self, nome, programas):
-        self.nome = nome
-        self.programas = programas
-    
-    def tamanho(self):
-        return len(self.programas)
+
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
-        super().__init__(nome,ano)
+        super().__init__(nome, ano)
         self.temporadas = temporadas
-    
+
     def __str__(self):
         return f'{self.nome} - {self.ano} -{self.temporadas} temporadas - {self.likes} Likes'
 
+
+class Playlist(list):
+    def __init__(self, nome, programas):
+        self.nome = nome
+        super().__init__(programas)
+
+
 atlanta = Serie("Atlanta", 2018, 2)
 vingadores = Filme("Vingadores - guerra infinita", 2018, 160)
-
+tmep = Filme("Todo mundo em panico", 1999, 100)
+demolidor = Serie("Demolidor", 2016, 2)
 
 atlanta.adicionar_like()
 atlanta.adicionar_like()
 vingadores.adicionar_like()
+tmep.adicionar_like()
+tmep.adicionar_like()
+tmep.adicionar_like()
+tmep.adicionar_like()
+demolidor.adicionar_like()
 
+filmes_e_series = [atlanta, demolidor, vingadores, tmep]
+playlist_fim_de_semana = Playlist("playlist_fim_de_semana", filmes_e_series)
 
-filmes_e_series = [vingadores, atlanta]
-for programa in filmes_e_series:
+for programa in playlist_fim_de_semana:
     print(programa)
